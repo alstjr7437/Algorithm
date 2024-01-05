470d05dacd0960687fbb3e57bb32d00d4ff6850b
김민석|Minseok Kim <alstjr7437@naver.com>
2024-01-05 11:19:04 +0900

    1764 해시 완료

---
+++ python/1764.py
@@ -0,0 +1,13 @@
+# 처음에 틀린 이유 : 사전순 정렬도 해주기!!
+n, m = map(int, input().split())
+result0 = set()
+result1 = set()
+for _ in range(n):
+    result0.add(input())
+for _ in range(m):
+    result1.add(input())
+
+result = sorted(list(result0 & result1))
+print(len(result))
+for i in result :
+    print(i)
