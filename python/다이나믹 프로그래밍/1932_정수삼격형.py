f371da10353ee4899839351392d630656336b24b
김민석|Minseok Kim <alstjr7437@naver.com>
2024-02-11 12:53:12 +0900

    1932 정수 삼각형(DP) sovled

---
+++ python/다이나믹 프로그래밍/1932_정수삼격형.py
@@ -0,0 +1,16 @@
+n = int(input())
+num_list = []
+
+for i in range(n):
+    num_list.append(list(map(int, input().split())))
+
+for i in range(1,n):
+    for j in range(len(num_list[i])):
+        if j == 0:
+            num_list[i][j] += num_list[i-1][0]
+        elif j == len(num_list[i]) - 1:
+            num_list[i][j] += num_list[i-1][j-1]
+        else :
+            num_list[i][j] = max(num_list[i][j] + num_list[i-1][j-1], num_list[i][j] + num_list[i-1][j])
+
+print(max(num_list[n-1]))
