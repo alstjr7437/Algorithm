a8506853a879786d1d0b62e6d20254910fcc5395
김민석|Minseok Kim <alstjr7437@naver.com>
2024-02-22 14:23:43 +0900

    벽 부수고 이동하기 solved

--- 백준/BFS/2206_벽-부수고-이동하기.py
+++ 백준/BFS/2206_벽-부수고-이동하기.py
@@ -36,3 +36,43 @@ def BFS():
     return -1
 
 print(BFS())
+
+"""
+from collections import deque
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+n, m = map(int, input().split())
+
+map = []
+for i in range(n):
+    map.append(list(input()))
+
+queue = deque()
+queue.append((0, 0, 0))
+
+result = [[0] * m  for _ in range(n)]
+result[0][0] = 1
+
+def BFS() :
+    while queue:
+        x, y, break_cnt = queue.pop()
+
+        if x == n - 1 and y == m - 1:
+            return result[x][y]
+
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            if new_x < 0 or new_x >= n or new_y < 0 or new_y >= m:
+                continue
+            if map[new_x][new_y] == "0" and result[new_x][new_y] == 0:
+                result[new_x][new_y] = result[x][y] + 1
+                queue.append([new_x, new_y, break_cnt])
+            elif map[new_x][new_y] == "1" and break_cnt == 0 :
+                result[new_x][new_y] = result[x][y] + 1
+                queue.append([new_x, new_y, 1])
+    return -1
+
+print(BFS())
+"""
