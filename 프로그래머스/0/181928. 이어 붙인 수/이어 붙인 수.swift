8457dd2b56f3381c646afa29619af5c4b38e7468
김민석|Minseok Kim <alstjr7437@naver.com>
2025-01-06 20:11:25 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.swift
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.swift
@@ -2,9 +2,9 @@ import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
     let even = num_list.filter { $0 % 2 == 0 }
-    let evenNumber = Int(even.map(String.init).joined()) ?? 0
+    let evenNumber = Int(even.map{ String($0) }.joined()) ?? 0
     let odd = num_list.filter { $0 % 2 != 0 }.map(String.init).joined()
-    let oddNumber = Int(odd.map(String.init).joined()) ?? 0
+    let oddNumber = Int(odd.map{ String($0) }.joined()) ?? 0
     
     return evenNumber + oddNumber
 }
