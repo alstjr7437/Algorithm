bbd2ba3aedaea9a5049f29ef79fbb34d416e3a5d
김민석|Minseok Kim <alstjr7437@naver.com>
2025-02-05 13:27:31 +0900

    [Silver III] Title: N과 M (4), Time: 20 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.swift
@@ -0,0 +1,19 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+let N = input[0]
+let M = input[1]
+
+var result: [Int] = []
+
+func dfs(_ now: Int){
+    if result.count == M {
+        print(result.map{ String($0) }.joined(separator: " "))
+        return
+    }
+    for i in now...N {
+        result.append(i)
+        dfs(i)
+        _ = result.popLast()!
+    }
+}
+
+dfs(1)
