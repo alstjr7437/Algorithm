22090bff32c8d7f2d0f758353402c35d087e69eb
김민석|Minseok Kim <alstjr7437@naver.com>
2025-03-29 18:00:11 +0900

    [Silver I] Title: 미로 탐색, Time: 12 ms, Memory: 80004 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by KimMinSeok on 4/13/24.
+//
+
+import Foundation
+
+// 1. 입력
+let NM = readLine()!.split(separator: " ").map { Int(String($0))! }
+var board:[[Int]] = []
+let N = NM[0] - 1
+let M = NM[1] - 1
+
+for _ in 0...N {
+    let line = readLine()!
+    board.append(line.map { Int(String($0))! })
+}
+
+// 2. 미로 탐색(BFS)
+let dx = [0,0,-1,1]
+let dy = [-1,1,0,0]
+
+var queue:[(Int,Int)] = [(0,0)]
+var idx = 0
+
+board[0][0] = 0
+while idx < queue.count {
+    let current = queue[idx]
+    idx += 1
+    
+    if current.0 == N && current.1 == M { break }
+    for i in 0..<4 {
+        let nx = current.0 + dx[i]
+        let ny = current.1 + dy[i]
+        
+        if nx > M || nx < 0 || ny > N || ny < 0 { continue }
+        if board[ny][nx] == 1 {
+            queue.append((nx,ny))
+            board[ny][nx] = board[current.1][current.0] + 1
+        }
+    }
+}
+
+// 3. 결과 출력
+print(board[N][M] + 1)
+
