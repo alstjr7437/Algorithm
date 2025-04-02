f04d48ebe40b21a44d290209a533e20ce1c7a587
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-02 17:16:58 +0900

    [Silver IV] Title: 개미, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/3048. 개미/개미.swift
+++ 백준/Silver/3048. 개미/개미.swift
@@ -1,18 +1,15 @@
-import Foundation
-
 let N = readLine()!.split(separator: " ").map { Int(String($0))! }
-
-let N1 = readLine()!
-let N2 = readLine()!
-
+let ant1 = readLine()!
+let ant2 = readLine()!
 let T = Int(readLine()!)!
-let ant1Last = N1.first!
 
-var result = Array(N1.reversed()) + Array(N2)
+let ant1Last = ant1.first!
+
+var result = Array(ant1.reversed()) + Array(ant2)
 
 for _ in 0..<T {
     for i in 0..<(result.count - 1) {
-        if N1.contains(result[i]) && N2.contains(result[i + 1]) {
+        if ant1.contains(result[i]) && ant2.contains(result[i + 1]) {
             result.swapAt(i, i+1)
             if result[i + 1] == ant1Last { break }
         }
