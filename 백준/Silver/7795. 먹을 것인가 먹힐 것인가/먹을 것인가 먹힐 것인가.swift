34843edc8b2a703f9fb89bc9f33510f57c80ae95
김민석|Minseok Kim <alstjr7437@naver.com>
2025-04-30 11:52:20 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 836 ms, Memory: 71628 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.swift
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.swift
@@ -2,17 +2,17 @@ let T = Int(readLine()!)!
 
 for _ in 0..<T {
     let NM = readLine()!.split(separator: " ").map { Int($0)! }
-    let A = readLine()!.split(separator: " ").map { Int($0)! }
-    let B = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = A.sorted()
-    let b = B.sorted()
+    var A = readLine()!.split(separator: " ").map { Int($0)! }
+    A.sort()
+    var B = readLine()!.split(separator: " ").map { Int($0)! }
+    B.sort()
     
     var point = 0
     var result = 0
-    for i in a {
+    for i in A {
         result += point
-        for j in point..<b.count {
-            if i > b[j] {
+        for j in point..<B.count {
+            if i > B[j] {
                 point = j + 1
                 result += 1
             }
