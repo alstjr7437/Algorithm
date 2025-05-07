f3647d09940b9373eddb9da16206df95c4876e36
김민석|Minseok Kim <alstjr7437@naver.com>
2025-05-07 18:25:53 +0900

    [Silver IV] Title: 알바생 강호, Time: 48 ms, Memory: 70672 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.swift
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.swift
@@ -1,4 +1,3 @@
-
 var visitor: [Int] = []
 
 for _ in 0..<Int(readLine()!)! {
@@ -9,14 +8,9 @@ visitor.sort(by: >)
 
 var result = 0
 
-var rank = 1
-
 for i in 0..<visitor.count {
-    let money = visitor[i] - ((rank) - 1)
-    if 0 < money {
-        result += money
-        rank += 1
-    }
+    let money = visitor[i] - ((i + 1) - 1)
+    if 0 < money { result += money }
 }
 
 print(result)
