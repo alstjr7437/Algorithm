398cf0676bc59ac1b44c7790ac32cc64c25d1125
김민석|Minseok Kim <alstjr7437@naver.com>
2025-05-23 00:24:38 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 135.15 ms, Memory: 42.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+func puzzle(diffs: [Int], times: [Int], level: Int) -> Int{
+    var result = 0
+    for i in 0..<diffs.count {
+        if diffs[i] <= level {
+            result += times[i]
+        } else {
+            let discorrectCount = diffs[i] - level
+            let discorrectUseTime = (i > 0 ? times[i - 1] : 0) + times[i]
+            let totalUseTime = discorrectUseTime * discorrectCount + times[i]
+            result += totalUseTime
+        }
+    }
+    
+    return result
+}
+
+
+func solution(_ diffs:[Int], _ times:[Int], _ limit:Int64) -> Int {
+    var start = 1
+    var end = diffs.max() ?? 10
+    var mid = 0
+    var answer = 0
+    
+    while start <= end {
+        mid =  (start + end) / 2
+        let result = puzzle(diffs: diffs, times: times, level: mid)
+        
+        if result <= limit {
+            answer = mid  
+            end = mid - 1     
+        } else {
+            start = mid + 1     
+        }
+    }
+    
+    return answer
+}
