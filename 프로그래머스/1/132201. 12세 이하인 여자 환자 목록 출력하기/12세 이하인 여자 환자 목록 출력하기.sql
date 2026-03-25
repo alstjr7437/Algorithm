8024535d8821c477455dad379da0bffa172d85c4
김민석|Minseok Kim <alstjr7437@naver.com>
2026-03-25 17:28:16 +0900

    [level 1] Title: 12세 이하인 여자 환자 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+select PT_NAME, PT_NO, GEND_CD, AGE, ifnull(TLNO, 'NONE') as TLNO
+from PATIENT
+where AGE <= 12 and GEND_CD = 'W'
+order by age desc, PT_NAME
+
+# select *
+# from PATIENT
