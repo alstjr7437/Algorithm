3f6414443be02a1c2606842e02356f171fe514be
김민석|Minseok Kim <alstjr7437@naver.com>
2026-04-02 19:43:26 +0900

    [level 1] Title: 최소직사각형, Time: 2.97 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ sizes:[[Int]]) -> Int {
+    
+    var width = 0
+    var height = 0
+    
+    for size in sizes {
+        width = max(max(size[0], size[1]), width)
+        height = max(height, min(size[0], size[1]))
+    }
+    
+    return width * height
+}
